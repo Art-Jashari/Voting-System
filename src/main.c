834e31da3f8f2bfbdeeb866bb4e98637907c5d8e
Art Jashari <art.jashari1@student.uni-pr.edu>
2024-04-24 23:39:14 +0200

    Project Structure and .gitignore Set Up

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Logic will be handled in a different header file.
+// Here is where that logic will run.
+
+int main() {
+    printf("Hello, World!\n");
+
+
+    return 0;
+}
