f8cebc85fc4bee9fdca08b2188aefa769fc0a02f
Art Jashari <art.jashari1@student.uni-pr.edu>
2024-04-25 00:13:29 +0200

    Updated election.h
    - Added some methods that will be used to handle and manipulate the data of the election.

--- include/election.h
+++ include/election.h
@@ -7,7 +7,7 @@
 #include <conio.h>
 #include <string.h>
 
-// Here is where the logic will be handled through corresponding methods
+// In this file is where the logic will be handled through corresponding methods
 
 #ifndef VOTING_SYSTEM_ELECTION_H
 #define VOTING_SYSTEM_ELECTION_H
@@ -26,6 +26,8 @@ typedef struct candidate{
     int votes;
 }CANDIDATE;
 
+
+
 // GLOBALS -------------------------------------------------------------------------------
 
 struct currentValidID currentValidId; // Stores current valid user ID parameters.
@@ -38,6 +40,31 @@ char studentVotes[200]; // Stores the information of the votes submited by each 
 
 
 
+// Here, the functions to handle the system's possible actions will be added.
+// FUNCTIONS -----------------------------------------------------------------------------
+
+
+void adminPanel(){}
+void studentPanel(){}
+
+int authenticateAdmin(){}
+int checkBranchCode(){  // Will check whether the inputed branch code is matching with the global branch code.
+
+}
+void banID(){}
+
+void saveVote(){}
+void deleteIllegalVote(){}
+
+void initiateNewElection(){}
+int getWinner(){}
+int extractRollNum(){}
+
+void createCandidateFiles(){}
+
+
+
+// ---------------------------------------------------------------------------------------
 
 
 
