855a4265bf49717fc18b3144fef6775d2c19b119
Art Jashari <art.jashari1@student.uni-pr.edu>
2024-04-28 11:57:57 +0200

    Update main.c
     - main.c logic designed and finalized.

--- src/main.c
+++ src/main.c
@@ -5,7 +5,28 @@
 // Here is where that logic will run.
 
 int main() {
-    printf("Hello, World!\n");
+    char input;
+
+    while (1) {
+        
+        printf("\n\t\t\t   1.Student panel \n\t\t\t   2.Admin panel \n\t\t\t   3.Exit \n\t\t\t   Option:");
+        scanf("%c", &input);
+
+        switch (input) {
+            case '1':
+                studentPanel();
+                break;
+            case '2':
+                adminPanel();
+                break;
+            case '3':
+                return 0;
+            default:
+                printf("\nInvalid option");
+                getch();
+
+        }
+    }
 
     return 0;
 }
